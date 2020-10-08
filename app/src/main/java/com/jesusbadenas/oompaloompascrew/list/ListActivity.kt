986eba4aa8a8da0bc565d74da95bdc0bb5e4c541
Jesús Badenas <chusobadenas@gmail.com>
2020-10-08 19:34:46 +0200

    Set toolbar

--- app/src/main/java/com/jesusbadenas/oompaloompascrew/list/ListActivity.kt
+++ app/src/main/java/com/jesusbadenas/oompaloompascrew/list/ListActivity.kt
@@ -2,12 +2,30 @@ package com.jesusbadenas.oompaloompascrew.list
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.jesusbadenas.oompaloompascrew.R
+import kotlinx.android.synthetic.main.toolbar.*
 
 class ListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_activity)
+
+        // Toolbar
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        val navController = navHostFragment.navController
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
